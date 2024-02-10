61c104ca61684a6bce46824d27ced3c03d00af98
Olya <olapancisina1@gmail.com>
2024-02-10 13:54:00 +0100

    add routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
 	return (
-		<header>Header</header>
+		<header>
+			<Link href='/'>Home</Link>
+			<Link href='/events/all'>All Events</Link>
+		</header>
 	)
 }
