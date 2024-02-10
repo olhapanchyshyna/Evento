9f0e9f805a4d356a7deda2bd0207c04ea51bd434
Olya <olapancisina1@gmail.com>
2024-02-10 14:40:26 +0100

    header styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return (
-    <></>
-  )
+	return (
+		<main>
+			<h1>Find events around you</h1>
+			<p>Browse more than 10,000 events around you</p>
+
+			<form>
+				<input placeholder='Search events in any city...' spellCheck={false} />
+			</form>
+
+			<section>
+				<p>Popular:</p>
+				<div className='space-x-2 font-semibold'>
+					<Link href='/events/austin'>Austin</Link>
+					<Link href='/events/seattle'>Seattle</Link>
+				</div>
+			</section>
+		</main>
+	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
 import Link from 'next/link'
-import React from 'react'
+import Logo from './Logo'
+
+const routes = [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'All Events',
+		path: '/events/all',
+	},
+]
 
 export default function Header() {
 	return (
-		<header>
-			<Link href='/'>Home</Link>
-			<Link href='/events/all'>All Events</Link>
+		<header className='flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9'>
+			<Logo />
+			<nav>
+				<ul className='flex gap-x-6 h-full text-sm'>
+					{routes.map(route => {
+						return (
+							<li className='text-white/50 hover:text-white transition' key={route.path}>
+								<Link href={route.path}>{route.name}</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</nav>
 		</header>
 	)
 }
