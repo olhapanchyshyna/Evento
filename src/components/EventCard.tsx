bee6b42248b0e424b9d4fdcb4b642f6589c47895
Olya <olapancisina1@gmail.com>
2024-02-11 13:18:16 +0100

    Event Date

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,28 +1,39 @@
-
 import { EventoEvents } from '@/lib/types'
-import Image from "next/image";
-
+import Image from 'next/image'
 
 type EventCardProps = {
-  event: EventoEvents;
-};
+	event: EventoEvents
+}
 
 export default function EventCard({ event }: EventCardProps) {
-  return (
-      <section className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects max-w-[500px]">
-        <Image
-          src={event.imageUrl}
-          alt={event.name}
-          width={500}
-          height={280}
-          className="h-[60%] object-cover"
-        />
+	return (
+		<section className='w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects max-w-[500px]'>
+			<Image
+				src={event.imageUrl}
+				alt={event.name}
+				width={500}
+				height={280}
+				className='h-[60%] object-cover'
+			/>
 
-        <div className="flex flex-col flex-1 justify-center items-center">
-          <h2 className="text-2xl font-semibold">{event.name}</h2>
-          <p className="italic text-white/75">By {event.organizerName}</p>
-          <p className="text-sm text-white/50 mt-4">{event.location}</p>
-        </div>
-      </section>
-  );
+			<div className='flex flex-col flex-1 justify-center items-center'>
+				<h2 className='text-2xl font-semibold'>{event.name}</h2>
+				<p className='italic text-white/75'>By {event.organizerName}</p>
+				<p className='text-sm text-white/50 mt-4'>{event.location}</p>
+			</div>
+
+			<section className='absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md'>
+      <p className="text-xl font-bold -mb-[5px]">
+            {new Date(event.date).toLocaleDateString("en-US", {
+              day: "2-digit",
+            })}
+          </p>
+          <p className="text-xs uppercase text-accent">
+            {new Date(event.date).toLocaleDateString("en-US", {
+              month: "short",
+            })}
+          </p>
+			</section>
+		</section>
+	)
 }
