473e816dc823cf5877836b2d62d1581d1a4a82d6
Olya <olapancisina1@gmail.com>
2024-02-11 10:35:08 +0100

    Scrollbar Styling

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function EventsPage() {
 	return (
-		<div>
+		<main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
 			<H1>All Events</H1>
-		</div>
+		</main>
 	)
 }
