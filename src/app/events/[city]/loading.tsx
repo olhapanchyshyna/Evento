243d3bdf1b70f369aacbc8539dabff4ba09a5a81
Olya <olapancisina1@gmail.com>
2024-02-13 11:02:07 +0100

    add skeleton card component

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Skeleton from '@/components/Skeleton'
+import SkeletonCard from '@/components/SkeletonCard'
 
 export default function loading() {
 	return (
-		<div>loading</div>
+		<div className='flex flex-wrap justify-center max-w-[1100px] mx-auto py-24 '>
+			<Skeleton className='h-12 w-[400px]' />
+			<div className='flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-24 gap-20'>
+				{Array.from({ length: 6 }).map((item, i) => (
+					<SkeletonCard key={i} />
+				))}
+			</div>
+		</div>
 	)
 }
