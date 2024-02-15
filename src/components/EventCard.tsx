0038932c0d7b6bd1d3a8649a8938feb84f6b687c
Olya <olapancisina1@gmail.com>
2024-02-15 10:05:31 +0100

    add animate for EventCard

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,14 +1,41 @@
+"use client"
+
 import { EventoEvents } from '@/lib/types'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRef } from 'react'
 
 type EventCardProps = {
 	event: EventoEvents
 }
+const MotionLink = motion(Link);
 
 export default function EventCard({ event }: EventCardProps) {
-	return (
-		<Link  className="flex-1 basis-80 h-[380px] max-w-[500px]" href={`/event/${event.slug}`}>
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
+  return (
+    <MotionLink
+      ref={ref}
+      className="flex-1 basis-80 h-[380px] max-w-[500px]"
+      href={`/event/${event.slug}`}
+      style={{
+        // @ts-ignore
+        scale: scaleProgress,
+        // @ts-ignore
+        opacity: opacityProgress,
+      }}
+      initial={{
+        opacity: 0,
+        scale: 0.8,
+      }}
+    >
 			<section className='w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects'>
 				<Image
 					src={event.imageUrl}
@@ -37,6 +64,6 @@ export default function EventCard({ event }: EventCardProps) {
 					</p>
 				</section>
 			</section>
-		</Link>
+		</MotionLink>
 	)
 }
